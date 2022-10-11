b1c7c1d23652dc6408679f484a631135b1582ae4
nansmatty <narayanmaitysp1997@gmail.com>
2022-10-11 21:43:02 +0530

    animation and filtering complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
+import Movie from "./component/Movie";
+import Filter from "./component/Filter";
+import {
+	motion,
+	AnimatePresence,
+} from "framer-motion";
 
 function App() {
+	const [popular, setPopular] = useState([]);
+	const [filtered, setFiltered] = useState([]);
+	const [activeGenre, setActiveGenre] =
+		useState(0);
+
+	useEffect(() => {
+		fetchPopular();
+	}, []);
+
+	const fetchPopular = async () => {
+		const { data } = await axios.get(
+			process.env.REACT_APP_MOVIE_API
+		);
+		setPopular(data.results);
+		setFiltered(data.results);
+	};
+
 	return (
-		<div className='App'>
-			<h1>Hello World!!!</h1>
+		<div>
+			<Filter
+				popular={popular}
+				setFiltered={setFiltered}
+				activeGenre={activeGenre}
+				setActiveGenre={setActiveGenre}
+			/>
+			<motion.div
+				layout
+				className='popular-movies'>
+				<AnimatePresence>
+					{filtered?.map((movie) => (
+						<Movie key={movie.id} movie={movie} />
+					))}
+				</AnimatePresence>
+			</motion.div>
 		</div>
 	);
 }
